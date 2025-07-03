6a4d727210c87b82c44cca41933c702761175313
Ugochukwu kenan Obinna <obinnakenan@yahoo.com>
2025-07-03 11:06:11 +0100

    Update zoopla_scraper.py

--- .github/workflows/zoopla_scraper.py
+++ .github/workflows/zoopla_scraper.py
@@ -22,7 +22,7 @@ def get_headless_driver():
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("user-agent=Mozilla/5.0")
+    chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36")
     return Chrome(options=chrome_options)
 
 def etext(e: WebElement) -> str:
